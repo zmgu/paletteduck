4a81dbfd7fbe21ba1c28f6502e7ec6ccd7ef06f1
zmgu <122958434+zmgu@users.noreply.github.com>
2025-11-28 16:34:25 +0900

    .

--- paletteduck-server/src/main/java/com/unduck/paletteduck/domain/game/dto/GameMode.java
+++
@@ -1,6 +0,0 @@
-package com.unduck.paletteduck.domain.game.dto;
-
-public enum GameMode {
-    NORMAL,
-    CUSTOM
-}
--- paletteduck-server/src/main/java/com/unduck/paletteduck/domain/game/dto/GameSettings.java
+++ paletteduck-server/src/main/java/com/unduck/paletteduck/domain/game/dto/GameSettings.java
@@ -14,7 +14,6 @@ public class GameSettings {
     private int maxPlayers = RoomConstants.DEFAULT_MAX_PLAYERS;
     private int rounds = RoomConstants.DEFAULT_ROUNDS;
     private int wordChoices = RoomConstants.DEFAULT_WORD_CHOICES;
-    private GameMode mode = GameMode.NORMAL;
     private int drawTime = RoomConstants.DEFAULT_DRAW_TIME;
     private int maxSpectators = RoomConstants.MAX_SPECTATORS;
 }
